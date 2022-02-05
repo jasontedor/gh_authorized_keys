6fe21f08ab232c196497615f05bdf183ce187ab5
Jason Tedor <jason@tedor.me>
2022-02-05 16:42:36 -0500

    Handle non-existent authorized_keys file
    This commit handles the case of the authorized_keys file not existing. This can
    be useful when running the script for the first time on a machine that does not
    already have authorized_keys configured.

--- gh_authorized_keys.sh
+++ gh_authorized_keys.sh
@@ -10,8 +10,13 @@ fi
 
 home=$(bash -c "cd ~$(printf %q "$1") && pwd")
 gh_keys=$(curl -sS https://github.com/"$2".keys)
-authorized_keys=$(cat "$home"/.ssh/authorized_keys)
+authorized_keys_file=$home/.ssh/authorized_keys
+if test -f "$authorized_keys_file"; then
+  authorized_keys=$(cat "$authorized_keys_file")
+else
+  authorized_keys=""
+fi
 if [ "$gh_keys" != "$authorized_keys" ]
 then
-  echo "$gh_keys" > "$home"/.ssh/authorized_keys
+  echo "$gh_keys" > "$authorized_keys_file"
 fi
